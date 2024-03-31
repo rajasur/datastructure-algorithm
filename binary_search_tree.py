b2e113f017b358484b9965392a9928c21d5ab353
I571352 <raja.sur@sap.com>
2024-03-31 09:26:20 +0530

    update

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -49,7 +49,7 @@ class BST:
         if self.key:
             print(self.key,end=" ")
             
-    #Searching the value is BST
+    #Searching the value in BST
     def search(self,data):
         if self.key==data:
             print("Data is found in the BST")
@@ -64,6 +64,40 @@ class BST:
                 self.rchild.search(data)
             else:
                 print("Data is not found.")
+                
+    #Deletion operation in BST
+    def delete(self,data):
+        if self.key is None:
+            print("Tree is empty!!")
+            return 
+        if self.key < data:
+            if self.rchild:
+                self.rchild=self.rchild.delete(data)
+            else:
+                print("Tree is empty!!")
+        elif self.key > data:
+            if self.lchild:
+                self.lchild=self.lchild.delete(data)
+            else:
+                print("Tree is Empty!!")
+        else:
+            #If O node is present in the both side
+            if self.lchild is None:
+                temp=self.rchild 
+                self=None 
+                return temp
+            #If 1 node is present in the both side
+            if self.rchild is None:
+                temp=self.lchild 
+                self=None 
+                return temp
+            #If both node is present in the both side 
+            node=self.rchild
+            while node.lchild:
+                node = node.lchild
+            self.key=node.key 
+            self.rchild=self.rchild.delete(node.key)
+        return self
 
 root=BST(10)
 l=[6,3,1,6,98,3,7]
@@ -76,3 +110,7 @@ print()
 root.post_order()
 print()
 root.search(10)
+print("Before Deleting")
+root.delete(6)
+print("After deleting")
+root.preorder()
